d098d272ac0cb95203c72cb865878cc47b66694e
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2018-05-09 10:12:33 -0400

    Cleanup infrastructure

--- Makefile
+++ Makefile
@@ -1,4 +1,19 @@
+.PHONY: docs build
+
+build:
+	python setup.py sdist
+
 clean:
 	find . -name '*.pyc' | xargs rm -Rif
 	find . -name '*egg-info' | xargs rm -Rif
 	find . -name __pycache__ | xargs rm -Rif
+	rm -Rif dist/
+	rm -Rif build/
+
+docs:
+	make -C docs/ html
+
+release:
+	make clean
+	make build
+	twine upload dist/*.tar.gz
--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ import pathlib
 import setuptools
 
 
+subpackage_name = 'ipython'
+
+
 def read_version():
     root_path = pathlib.Path(__file__).parent
     version_path = root_path / 'abjadext' / subpackage_name / 'version.py'
@@ -13,58 +16,43 @@ def read_version():
     return local_dict['__version__']
 
 
-subpackage_name = 'ipython'
-
-author = 'Josiah Wolf Oberholtzer'
-
-author_email = 'josiah.oberholtzer@gmail.com'
-
-classifiers = [
-    'Development Status :: 5 - Production/Stable',
-    'License :: OSI Approved :: GNU General Public License (GPL)',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: Implementation :: CPython',
-    'Topic :: Artistic Software',
-    ]
-
-install_requires = [
-    'abjad>=2.21',
-    'jupyter==1.0.0',
-    ]
-
-keywords = [
-    'music composition',
-    'music notation',
-    'formalized score control',
-    'lilypond',
-    ]
-keywords += [
-    'documentation',
-    'ipython',
-    ]
-keywords = ', '.join(keywords)
-
-with open('README.md', 'r') as file_pointer:
-    long_description = file_pointer.read()
-
-version = read_version()
-
-
 if __name__ == '__main__':
     setuptools.setup(
-        author=author,
-        author_email=author_email,
-        classifiers=classifiers,
+        author='Josiah Wolf Oberholtzer',
+        author_email='josiah.oberholtzer@gmail.com',
+        classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'License :: OSI Approved :: GNU General Public License (GPL)',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: Implementation :: CPython',
+            'Topic :: Artistic Software',
+            ],
+        extras_require={
+            'development': [
+                'mypy',
+                'pytest-helpers-namespace',
+                'pytest>=3.5.0',
+                ],
+            },
         include_package_data=True,
-        install_requires=install_requires,
+        install_requires=[
+            'abjad>=2.21',
+            'jupyter>=1.0.0',
+            'uqbar>=0.2.11',
+            ],
         license='MIT',
-        long_description=long_description,
-        keywords=keywords,
+        long_description=pathlib.Path('README.md').read_text(),
+        keywords=', '.join([
+            'music composition',
+            'music notation',
+            'formalized score control',
+            'lilypond',
+            'documentation',
+            'cli',
+            ]),
         name='abjad-ext-{}'.format(subpackage_name),
         packages=['abjadext'],
         platforms='Any',
         url='http://www.projectabjad.org',
-        version=version,
-        )
+        version=read_version(),
+    )
